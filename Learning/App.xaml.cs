f77ea662509ed1b1dab34aeb53a355392ab66569
Jcardif <josh.cardif14@outlook.com>
2018-05-15 11:02:25 +0300

    Complete legendary challenge

--- Learning/App.xaml.cs
+++ Learning/App.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Learning.Pages;
 
 namespace Learning
 {
@@ -66,7 +67,7 @@ namespace Learning
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(LegengaryChallenge), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
@@ -95,11 +96,6 @@ namespace Learning
             var deferral = e.SuspendingOperation.GetDeferral();
             //TODO: Save application state and stop any background activity
             deferral.Complete();
-        }
-
-        private void btn_Click()
-        {
-
         }
     }
 }
