19716f08d473e9265ad4ec5dca74dc9fe30d6d9e
Jcardif <josh.cardif14@outlook.com>
2018-06-02 10:57:58 +0300

    Add Book.cs

--- Learning/App.xaml.cs
+++ Learning/App.xaml.cs
@@ -69,7 +69,7 @@ namespace Learning
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(Stupendous_Style_Challenge), e.Arguments);
+                    rootFrame.Navigate(typeof(VisualStateManagerTask), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
