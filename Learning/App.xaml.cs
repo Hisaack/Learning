a4ca61be786030fbd415de8410792d3311fcad21
Jcardif <josh.cardif14@outlook.com>
2018-06-02 19:04:51 +0300

    data bind

--- Learning/App.xaml.cs
+++ Learning/App.xaml.cs
@@ -69,7 +69,7 @@ namespace Learning
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(DataBindingInGridVew), e.Arguments);
+                    rootFrame.Navigate(typeof(KeepingDataControlsUpdated), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
---
+++ Learning/Models/AvatarIcons.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning.Models
+{
+    public class AvatarIcon
+    {
+        public string IconPath { get; set; }
+
+    }
+
+    public static class Icon
+    {
+        public static List<AvatarIcon> LoadAvatarIcons()
+        {
+            var avatarIcons = new List<AvatarIcon>()
+            {
+                new AvatarIcon(){IconPath = "ms-appx:///Assets/female-01.png"},
+                new AvatarIcon(){IconPath = "ms-appx:///Assets/female-02.png"},
+                new AvatarIcon(){IconPath = "ms-appx:///Assets/female-03.png"},
+                new AvatarIcon(){IconPath = "ms-appx:///Assets/male-01.png"},
+                new AvatarIcon(){IconPath = "ms-appx:///Assets/male-02.png"},
+                new AvatarIcon(){IconPath = "ms-appx:///Assets/male-03.png"}
+            };
+            return avatarIcons;
+        }
+        
+    }
+}
